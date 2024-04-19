45b19a764480001d00293ab525b94445d0412bf1
Arigi <muhammad20149@mail.unpad.ac.id>
2024-04-19 20:59:35 +0700

    init project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -14,16 +14,19 @@ function Gradient({
   className?: string;
 }): JSX.Element {
   return (
-    <span
-      className={[
-        styles.gradient,
-        conic ? styles.glowConic : undefined,
-        small ? styles.gradientSmall : styles.gradientLarge,
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    />
+    <>
+      <span
+        className={[
+          styles.gradient,
+          conic ? styles.glowConic : undefined,
+          small ? styles.gradientSmall : styles.gradientLarge,
+          className,
+        ]
+          .filter(Boolean)
+          .join(" ")}
+      />
+      <div className="bg-red">Hello</div>
+    </>
   );
 }
 
